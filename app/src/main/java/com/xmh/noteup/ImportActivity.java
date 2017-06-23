174174266a3707e213f164517275cef12801b63b
voidXMH <xmh19936688@163.com>
2017-06-23 15:53:08 +0800

    move file-process into thread

--- app/src/main/java/com/xmh/noteup/ImportActivity.java
+++ app/src/main/java/com/xmh/noteup/ImportActivity.java
@@ -1,6 +1,7 @@
 package com.xmh.noteup;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
@@ -18,17 +19,47 @@ import java.util.List;
 
 public class ImportActivity extends AppCompatActivity {
 
+    private static final String TAG_CSV_FILE_PATH = "path";
+
+    private String filePath = "";
+    private List<Pair<String, Date>> list = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import);
 
         Intent intent = getIntent();
-        File file = new File(intent.getData().getPath());
+        filePath = intent.getData().getPath();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                File file = new File(filePath);
+                list = getInfoFromFile(file);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (list.size() > 0) {
+                            SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+                            SharedPreferences.Editor edit = preferences.edit();
+                            edit.putString(TAG_CSV_FILE_PATH, filePath);
+                        }
+                    }
+                });
+
+            }
+        }).start();
+    }
+
+    private List<Pair<String, Date>> getInfoFromFile(File file) {
         List<Pair<String, Date>> list = new ArrayList<>();
         SimpleDateFormat dateFormater = new SimpleDateFormat("yyyy/M/d");
+
+        BufferedReader reader = null;
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "gb2312"));
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "gb2312"));
             String line = "";
             while ((line = reader.readLine()) != null) {
                 if (line.split(",").length == 27) {
@@ -40,11 +71,18 @@ public class ImportActivity extends AppCompatActivity {
                     }
                 }
             }
-            list.size();
+
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
-
+        return list;
     }
 }
